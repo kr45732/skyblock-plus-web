f1214d1e1f38112bee4e773dac237b0b88887594
TrashCan69420 <trashcan69666@gmail.com>
2021-12-19 03:27:09 -0500

    remove useless comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
   ],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {},
   },
