0e7afa7a97248020880591e18811eabe9c0755db
TrashCan69420 <trashcan69666@gmail.com>
2021-12-19 14:51:48 -0500

    thong

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-screen bg-dark-gray">
+    <NavBar />
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "PrivacyPolicyPage",
+});
+</script>
