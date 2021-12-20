beaa6a2c46921b0e3885e255b42ec1cf06a2a618
TrashCan69420 <trashcan69666@gmail.com>
2021-12-19 20:24:06 -0500

    dont remember what i changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
+  mode: "jit",
   purge: [
     "./components/**/*.{vue,js}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
   ],
   darkMode: false,
   theme: {
