ae7cbba81cff3674d99f8ccf55b6e56a944ff4bb
TrashCan <trashcan69666@gmail.com>
2021-12-31 20:38:29 -0500

    Commands + misc fixes
    :)

---
+++ src/routes/commands.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Navbar from "../components/navbar.svelte";
+  import Footer from "../components/footer.svelte";
+</script>
+
+<div class="bg-black h-screen text-white">
+  <Navbar />
+  <div>
+    <h1>commands</h1>
+  </div>
+  <Footer />
+</div>
