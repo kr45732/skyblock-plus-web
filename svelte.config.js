9203d41eadbe1b8494ac0e7f746edc5d3c9bc3e2
TrashCan <trashcan69666@gmail.com>
2021-12-31 20:15:33 -0500

    Privacy Policy & TOS stuff(wip)
    :)

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
+import mdsvexConfig from "./mdsvex.config.js";
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [preprocess({})],
+  extensions: [".svelte", ...mdsvexConfig.extensions],
+  preprocess: [preprocess({}), mdsvex(mdsvexConfig)],
 
   kit: {
     adapter: adapter(),
