4cfc36b81bed252e536b5ad6ce82058a26b24ea3
TrashCan <trashcan69666@gmail.com>
2021-12-31 23:04:46 -0500

    my brain hurts

---
+++ src/routes/stats.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Navbar from "../components/navbar.svelte";
+  import Footer from "../components/footer.svelte";
+  import { onMount } from "svelte";
+
+  let stats;
+  onMount(async () => {
+    let data = await fetch(
+      "https://api.allorigins.win/get?url=https://skyblock-plus.ml/api/public/get/stats"
+    ).then((res) => res.json());
+    stats = data;
+  });
+</script>
+
+<div>
+  <div class="bg-black h-screen text-white">
+    <Navbar />
+    {stats.guild_count}
+    
+    {stats.user_count}
+    <Footer />
+  </div>
+</div>
