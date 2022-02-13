48590f8b295ea9f11f300d01cfd5f92e50fc450f
kr45732 <52721908+kr45732@users.noreply.github.com>
2022-02-13 11:29:08 -0500

    eslint moment

--- src/routes/commands.svelte
+++ src/routes/commands.svelte
@@ -1,12 +1,70 @@
 <script>
   import Navbar from "../components/navbar.svelte";
   import Footer from "../components/footer.svelte";
+  import { onMount } from "svelte";
+
+  let search = undefined;
+  let commands = [];
+
+  $: filteredCommands = search
+    ? commands.filter((command) => command.name.match(`${search}.*`))
+    : commands;
+
+  onMount(async function () {
+    const response = await fetch(
+      "https://api.allorigins.win/get?url=https://skyblock-plus.ml/api/public/get/commands"
+    );
+    const unparsed_json = await response.json();
+    commands = JSON.parse(unparsed_json.contents);
+  });
 </script>
 
-<div class="bg-black h-screen text-white">
+<div class="bg-black h-screen text-white overflow-scroll">
   <Navbar />
-  <div>
-    <h1>commands</h1>
-  </div>
+  {#if commands.length > 0}
+    <div style="padding: 15px;">
+      <input
+        type="search"
+        bind:value={search}
+        class="ms-auto w-auto"
+        style="color: black; padding-left: 10px; margin-bottom: 10px;"
+        placeholder="Search for a command..."
+      />
+
+      <table style="width: 100%;">
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Usage</th>
+          <th>Aliases</th>
+        </tr>
+        {#each filteredCommands as command}
+          <tr>
+            <td>{command?.name}</td>
+            <td>{command?.description}</td>
+            <td>{command?.usage}</td>
+            <td>{command?.aliases}</td>
+          </tr>
+          {#each command.subcommands as subcommand}
+            <tr>
+              <td>{command?.name} {subcommand?.name}</td>
+              <td>{subcommand?.description}</td>
+              <td>{command?.usage} {subcommand?.usage}</td>
+              <td>{subcommand?.aliases}</td>
+            </tr>
+          {/each}
+        {/each}
+      </table>
+    </div>
+  {:else}
+    <h1>Loading..</h1>
+  {/if}
   <Footer />
 </div>
+
+<style>
+  th,
+  td {
+    border: 1px solid white;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,16 @@
 <script>
   import Navbar from "../components/navbar.svelte";
   import Footer from "../components/footer.svelte";
+  import { onMount } from "svelte";
+  let stats = {};
+
+  onMount(async function () {
+    const response = await fetch(
+      "https://api.allorigins.win/get?url=https://skyblock-plus.ml/api/public/get/stats"
+    );
+    const unparsed_json = await response.json();
+    stats = JSON.parse(unparsed_json.contents);
+  });
 </script>
 
 <div>
@@ -15,8 +25,24 @@
             SkyBlock players and guild staff!
           </p>
           <button class="btn bg-red-700 hover:bg-red-600 border-red-700 hover:border-red-600"
-            ><a href="https://dsc.gg/sb+">Inivte Me</a></button
+            ><a href="https://dsc.gg/sb+">Invite Me</a></button
           >
+          <div class="flex justify-center mb-0 p-12 grid-flow-row space-x-14">
+            <div
+              class="relative object-scale-down subpixel-antialiased px-4 py-2 font-extrabold text-xl text-center rounded-lg bg-red-700 w-36 h-18 text-white"
+            >
+              Servers<br />
+              <h3 class="subpixel-antialiased font-semibold">
+                {stats.guild_count || "Loading..."}
+              </h3>
+            </div>
+            <div
+              class="relative object-scale-down subpixel-antialiased px-4 py-2 font-extrabold text-xl text-center rounded-lg bg-red-700 w-36 h-18 text-white"
+            >
+              Users<br />
+              <h3 class="subpixel-antialiased font-semibold">{stats.user_count || "Loading..."}</h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
