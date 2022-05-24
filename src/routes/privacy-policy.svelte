b1d6d6245257db024bf3ad111fd7aea9b58bca61
kr45732 <52721908+kr45732@users.noreply.github.com>
2022-05-24 19:43:04 -0400

    https://markdowntohtml.com/

---
+++ src/routes/privacy-policy.svelte
@@ -0,0 +1,77 @@
+<div>
+  <h1 id="privacy-policy">Privacy Policy</h1>
+  <p>
+    Your privacy is important to us. It is Skyblock Plus&#39;s policy to respect your privacy and
+    comply with any applicable law and regulation regarding any personal information we may collect
+    about you, including the bot, across our website, and other applications we own and operate.
+  </p>
+  <h2 id="collected-information">Collected Information</h2>
+  <p>
+    Information we collect includes both information you knowingly and actively provide us when
+    using or participating in any of our services, and any information automatically sent via
+    Discord in the course of accessing our products and services.
+  </p>
+  <h3 id="-logged-information-"><strong>Logged Information</strong></h3>
+  <p>
+    Our servers may log data provided by Discord when you use the bot. This may include the
+    server&#39;s name &amp; ID, user&#39;s name and ID, the timestamp of your use, and any errors
+    that may arise when using the bot.
+  </p>
+  <p>We log certain personal information which may include one or more of the following:</p>
+  <ul>
+    <li>Username, User Tag &amp; User ID</li>
+    <li>Server Name &amp; Server ID</li>
+  </ul>
+  <h3 id="-stored-information-"><strong>Stored Information</strong></h3>
+  <p>
+    When the our joins a server, we automatically store information that may include the server ID
+    &amp; server name. Other information stored may include information provided when configuring
+    features of the bot.
+  </p>
+  <h2 id="legitimate-reasons-for-processing-your-personal-information">
+    Legitimate Reasons for Processing Your Personal Information
+  </h2>
+  <p>
+    We only collect and use your personal information when we have a legitimate reason for doing so.
+    In which instance, we only collect personal information that is reasonably necessary to provide
+    our services to you.
+  </p>
+  <h2 id="removal-of-data">Removal of Data</h2>
+  <h3 id="-automatic-removal-"><strong>Automatic removal</strong></h3>
+  <p>
+    Stored Data can be removed automatically through means of removing the bot from a Server. This
+    can be achieved by either kicking or banning the bot from the server.
+  </p>
+  <h3 id="-manual-removal-"><strong>Manual removal</strong></h3>
+  <p>
+    Manual removal of the data can be requested by contacting using the methods provided below. For
+    security reasons will we ask you to provide us with proof of ownership of the server, that you
+    wish the data to be removed of. Only a server owner may request manual removal of data.
+  </p>
+  <h2 id="children-s-privacy">Children’s Privacy</h2>
+  <p>
+    We do not aim any of our products or services directly at children under the age of 13, and we
+    do not knowingly collect personal information about children under 13.
+  </p>
+  <h2 id="limits-of-our-policy">Limits of Our Policy</h2>
+  <p>
+    The bot may link to external sites that are not operated by us. Please be aware that we have no
+    control over the content and policies of those sites, and cannot accept responsibility or
+    liability for their respective privacy practices.
+  </p>
+  <h2 id="changes-to-this-policy">Changes to this Policy</h2>
+  <p>
+    At our discretion, we may change our privacy policy to reflect updates to our business
+    processes, current acceptable practices, or legislative or regulatory changes. Any modifications
+    made to the policy will be reflected here.
+  </p>
+  <h2 id="contact-us">Contact Us</h2>
+  <p>
+    For any questions or concerns regarding your privacy, you may contact us using the following
+    details:
+  </p>
+  <ul>
+    <li>Direct | CrypticPlasma#0820</li>
+    <li>Server | dsc.gg/skyblock-plus</li>
+  </ul>
+</div>
---
+++ src/routes/tos.svelte
@@ -0,0 +1,39 @@
+<div>
+  <h1 id="terms-and-conditions">Terms and Conditions</h1>
+  <h2 id="terms">Terms</h2>
+  <p>
+    By using this bot, you are agreeing to be bound by these Terms and Conditions and agree that you
+    are responsible for the agreement with any applicable local laws. If you disagree with any of
+    these terms, you are prohibited from accessing this site or bot.
+  </p>
+  <h2 id="affiliation">Affiliation</h2>
+  <p>
+    The bot is not affiliated with, supported, or made by Discord Inc. Any direct connection to
+    Discord or any of its trademark objects is purely coincidental. We do not claim to have the
+    copyright ownership of any of Discord&#39;s assets, trademarks or other intellectual property.
+  </p>
+  <h2 id="use-license">Use License</h2>
+  <p>
+    Permission is granted to temporarily download one copy of the materials on our bot for personal,
+    non-commercial transitory viewing only.
+  </p>
+  <h2 id="disclaimer">Disclaimer</h2>
+  <p>
+    All the materials on our bot are provided &quot;as is&quot;. We make no warranties, may it be
+    expressed or implied, therefore negates all other warranties. Furthermore, we do not make any
+    representations concerning the accuracy or reliability of the use of the materials on its bot or
+    otherwise relating to such materials or any sites linked to this bot. We are not held
+    accountable for any damages that will arise during the use of the bot.
+  </p>
+  <h2 id="links">Links</h2>
+  <p>
+    We have not reviewed all of the sites linked to its bot and is not responsible for the contents
+    of any such linked site. The use of any linked website is at the user’s own risk.
+  </p>
+  <h2 id="changes-to-this-policy">Changes to this Policy</h2>
+  <p>
+    At our discretion, we may change our terms and services to reflect updates to our business
+    processes, current acceptable practices, or legislative or regulatory changes. Any changes made
+    to the terms and services will be reflected here.
+  </p>
+</div>
