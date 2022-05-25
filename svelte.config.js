0e70e03f65a26a42c6c2dc1e4807357b39a185d7
kr45732 <52721908+kr45732@users.noreply.github.com>
2022-05-24 21:33:23 -0400

    remove unnecessary dependency

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import mdsvexConfig from "./mdsvex.config.js";
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
-import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [".svelte", ...mdsvexConfig.extensions],
-  preprocess: [preprocess({}), mdsvex(mdsvexConfig)],
+  extensions: [".svelte"],
+  preprocess: [preprocess({})],
   kit: {
     adapter: adapter()
   }
